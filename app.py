fec9d1def46f41a298246726fbcc00fede59d243
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-22 15:38:48 +0530

    create task endpoint created

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
 from dotenv import load_dotenv
 import os
+from pymongo.errors import PyMongoError
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,6 +11,22 @@ app = Flask(__name__)
 
 # Get MongoDB URI from environment variables
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route("/")
+def Home():
+    return "<h1>Hello world</h1>"
+
+
+@app.route("/v1/task", methods=["POST"])
+def create_task():
+    if(request.method == "POST"):
+        try:
+            res = mongo.db.task.insert_one({"title": request.json['title'], "isTaskCompleted": False})
+            return jsonify({"id": str(res.inserted_id)}), 201
+        except PyMongoError as e:
+            return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
