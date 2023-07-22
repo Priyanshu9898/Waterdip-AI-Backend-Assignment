849e7333e7787ba045f9993e09fafc9a5be1abe7
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-22 19:12:59 +0530

    get all tasks and get task by id is done

--- app.py
+++ app.py
@@ -25,32 +25,56 @@ def create_task():
 
             data['id'] = id_counter
 
-            if(data.get('is_completed') is None):
-               data['is_completed'] = False
+            if (data.get('is_completed') is None):
+                data['is_completed'] = False
 
             tasks_to_create.append(data)
             id_counter += 1
 
-            return jsonify({"id": data['id']}), 201 # Returns the created task's id 
+            tasks.extend(tasks_to_create)
+            created_ids = {'id': data['id']}
+           
+            return jsonify({"id": data['id']}), 201
 
         else:
-            tasks = data['tasks']
+            tasks_data = data['tasks']
 
-            for task in tasks:
+            for task_data in tasks_data:
 
-                if 'title' not in task:
+                if 'title' not in task_data:
                     return jsonify({'error': 'Missing task title'}), 400
-                
-                task['id'] = id_counter
-                if(task.get('is_completed') is None):
-                    task['is_completed'] = False
-          
-                tasks_to_create.append(task)
+
+                task_data['id'] = id_counter
+                if (task_data.get('is_completed') is None):
+                    task_data['is_completed'] = False
+
+                tasks_to_create.append(task_data)
                 id_counter += 1
 
             tasks.extend(tasks_to_create)
             created_ids = {'ids': [task['id'] for task in tasks_to_create]}
             return jsonify(created_ids), 201
+
+
+@app.route('/v1/tasks', methods=['GET'])
+def list_all_tasks():
+    try:
+        return jsonify({'tasks': tasks}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    try:
+        for task in tasks:
+            if task['id'] == task_id:
+                return jsonify(task), 200
+
+        return jsonify({'error': 'There is no task at that id'}), 404
+
+    except Exception as e:
+        return jsonify({'error': 'Unexpected error occurred'}), 500
 
 
 
