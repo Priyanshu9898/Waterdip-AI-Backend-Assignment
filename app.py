a98dcaf6af937ee0c9f4954c99477e544d3445b1
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-07-22 17:29:15 +0530

    Edit task endpoint added

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ def Home():
     return "<h1>Hello world</h1>"
 
 # Create a Task
+
+
 @app.route("/v1/tasks", methods=["POST"])
 def create_task():
     if (request.method == "POST"):
@@ -55,13 +57,40 @@ def get_task(id):
             doc = mongo.db.task.find_one({"_id": ObjectId(id)})
 
             if (doc is None):
-                return jsonify({'error': 'There is no task with that id'}), 404
+                return jsonify({'error': 'There is no task at that id'}), 404
             else:
                 return jsonify({"id": str(ObjectId(doc["_id"])), "title": doc["title"], "is_completed": doc["is_completed"]})
 
         except InvalidId as e:
             return jsonify({'error': 'Invalid id format'}), 400
-        
+
+        except PyMongoError as e:
+            return jsonify({'error': str(e)}), 500
+
+
+@app.route("/v1/tasks/<id>", methods=['PUT'])
+def update_task(id):
+    if request.method == "PUT":
+        try:
+
+            existed_task = mongo.db.task.find_one({"_id": ObjectId(id)})
+
+            if (existed_task is not None):
+
+                title = request.json.get("title", existed_task['title'])
+                is_completed = request.json.get(
+                    "is_completed", existed_task['is_completed'])
+
+                mongo.db.task.update_one({"_id": ObjectId(id)}, {'$set': {'title': title,
+                                                                          'is_completed': is_completed
+                                                                          }
+                                                                 })
+                return jsonify({}), 204
+            else:
+                return jsonify({"error": "There is no task at that id"}), 404
+
+        except InvalidId as e:
+            return jsonify({'error': 'Invalid id format'}), 400
         except PyMongoError as e:
             return jsonify({'error': str(e)}), 500
 
